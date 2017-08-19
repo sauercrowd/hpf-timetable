29103e632cc4ace38ca72451d92362bd8da5c257
sauercrowd <jonadev95@posteo.org>
2017-08-19 13:57:59 +0200

    minimal update, setup db

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"database/sql"
+	"fmt"
 
 	"github.com/sauercrowd/hpf-timetable/pkg/flags"
 )
@@ -12,9 +13,15 @@ type Storage struct {
 
 func NewStorage(flags *flags.Flags) (*Storage, error) {
 	var ret Storage
-
+	db, err := newPostgres(flags)
+	if err != nil {
+		return nil, err
+	}
+	ret.db = db
+	return &ret, nil
 }
 
 func newPostgres(flags *flags.Flags) (*sql.DB, error) {
-
+	dbstr := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable", flags.PostgresUser, flags.PostgresPass, flags.PostgresHost, flags.PostgresPort, "hpf-timetable")
+	return sql.Open("postgres", dbstr)
 }
